05c25f2ff00d65898ee0a3dcea7e463729e5544e
SuhelaMaloku <sm72824@ubt-uni.net>
2025-12-09 06:52:18 -1100

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,4 +1,4 @@
-const loginForm = document.getElementById('login-form');
+const loginForm = document.getElementById('loginForm');
 if(loginForm){
     loginForm.addEventListener('submit', function(e) {
         e.preventDefault();
@@ -8,4 +8,19 @@ if(loginForm){
             return alert('Please fill in all fields.');
         alert('Login successful!');
         window.location.href = 'Cineluxe.html';
+});
+}
+const registerForm = document.getElementById('registerForm');
+if(registerForm){
+    registerForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('regEmail').value;
+        const password = document.getElementById('regPassword').value;
+        const confirm=document.getElementById('confirmPassword').value;
+        if(!username || !email || !password || !confirm) return alert('Please fill in all fields.');
+        if(password !== confirm) return alert('Passwords do not match.');
+        alert('Registration successful!');
+        window.location.href = 'Login.html';
+    });
 }
