84685c588dca6c5cd959262794951bc70e89b2c9
SuhelaMaloku <sm72824@ubt-uni.net>
2026-01-12 03:38:37 -1100

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cineluxe_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Failed: " . $conn->connect_error);
+}
+?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cineluxe_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Failed: " . $conn->connect_error);
+}
+?>
