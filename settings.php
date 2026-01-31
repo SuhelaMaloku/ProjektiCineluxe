487025355f9ed62a576d93928b19f71128dc0fc0
SuhelaMaloku <sm72824@ubt-uni.net>
2026-01-31 07:55:52 -1100

    Update settings.php

--- settings.php
+++ settings.php
@@ -208,3 +208,21 @@ menuLinks.forEach(link => {
     });
 });
 </script>
+<script>
+function triggerClick() {
+    document.querySelector('#profileInput').click();
+}
+function displayImage(e) {
+    if (e.files && e.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function(event) {
+            document.querySelector('#userProfilePic').setAttribute('src', event.target.result);
+        }
+        
+        reader.readAsDataURL(e.files[0]);
+    }
+}
+</script>
+</body>
+</html>
